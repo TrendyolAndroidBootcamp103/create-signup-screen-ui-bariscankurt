b223e1125b36a6423c509afa1e951c4149c45d87
bariscankurt <bariscankurt0@gmail.com>
2021-04-30 12:58:59 -0700

    fixed validation issues

--- app/src/main/java/school/cactus/succulentshop/EmailValidator.kt
+++ app/src/main/java/school/cactus/succulentshop/EmailValidator.kt
@@ -4,13 +4,15 @@ class EmailValidator: Validator {
 
     override fun validateLogin(field: String): Int? = when{
         field.isEmpty() -> R.string.this_field_is_required
-        field.length < 5 -> R.string.identifier_is_too_short
+        field.length <= 5 -> R.string.email_is_invalid
+        field.length >= 50 -> R.string.email_is_invalid
+        field.contains("@",ignoreCase = true).not() -> R.string.email_is_invalid
         else -> null
     }
 
     override fun validateSingIn(field: String): Int? = when{
         field.isEmpty() -> R.string.email_is_required
-        field.length < 5 || field.length > 20 -> R.string.email_is_invalid
+        field.length <= 5 || field.length >= 50 -> R.string.email_is_invalid
         field.contains("@",ignoreCase = true).not() -> R.string.email_is_invalid
         else -> null
     }
